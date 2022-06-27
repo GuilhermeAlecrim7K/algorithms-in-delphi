f500c0d7c0161de016d2628215fb8fde78769ec8
Guilherme Alecrim <guilhermealecrim96@gmail.com>
2022-06-26 22:39:55 -0300

    <feat> ChildFormTemplate: Created panel to serve as parent for the components to be implemented in the child classes

--- src/View/Algorithms.View.Form.ChildTemplate.dfm
+++ src/View/Algorithms.View.Form.ChildTemplate.dfm
@@ -2,8 +2,8 @@ object FormChildTemplate: TFormChildTemplate
   Left = 0
   Top = 0
   Align = alClient
-  ClientHeight = 542
-  ClientWidth = 815
+  ClientHeight = 1469
+  ClientWidth = 744
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -12,6 +12,66 @@ object FormChildTemplate: TFormChildTemplate
   Font.Style = []
   OldCreateOrder = False
   Position = poDesigned
+  OnMouseWheelDown = FormMouseWheelDown
+  OnMouseWheelUp = FormMouseWheelUp
   PixelsPerInch = 96
   TextHeight = 13
+  object PnlBody: TPanel
+    Left = 0
+    Top = 57
+    Width = 744
+    Height = 1412
+    Align = alClient
+    BevelOuter = bvNone
+    Padding.Right = 5
+    Padding.Bottom = 5
+    ParentBackground = False
+    TabOrder = 1
+    OnResize = PnlBodyResize
+    DesignSize = (
+      744
+      1412)
+    object Scrollbar: TScrollBar
+      Left = 724
+      Top = 0
+      Width = 15
+      Height = 1495
+      Align = alRight
+      Kind = sbVertical
+      PageSize = 1
+      TabOrder = 1
+      OnScroll = ScrollbarScroll
+      ExplicitLeft = 719
+      ExplicitHeight = 453
+    end
+    object PnlContents: TPanel
+      Left = 1
+      Top = 47
+      Width = 722
+      Height = 1500
+      Anchors = []
+      BevelOuter = bvNone
+      Ctl3D = True
+      Padding.Right = 5
+      Padding.Bottom = 5
+      ParentBackground = False
+      ParentCtl3D = False
+      TabOrder = 0
+      ExplicitTop = 0
+    end
+  end
+  object Pnltop: TPanel
+    Left = 0
+    Top = 0
+    Width = 744
+    Height = 57
+    Align = alTop
+    BevelOuter = bvNone
+    Padding.Left = 18
+    Padding.Top = 10
+    Padding.Right = 15
+    Padding.Bottom = 10
+    ParentBackground = False
+    TabOrder = 0
+  end
 end
--- src/View/Algorithms.View.Form.ChildTemplate.pas
+++ src/View/Algorithms.View.Form.ChildTemplate.pas
@@ -3,22 +3,34 @@
 interface
 
 uses
-  Winapi.Windows, 
-  Winapi.Messages, 
+  System.Classes, 
   System.SysUtils, 
   System.Variants, 
-  System.Classes, 
-  Vcl.Graphics,
+  Vcl.Buttons,
   Vcl.Controls, 
-  Vcl.Forms, 
   Vcl.Dialogs, 
+  Vcl.ExtCtrls, 
+  Vcl.Forms, 
+  Vcl.Graphics,
   Vcl.StdCtrls, 
-  Vcl.ExtCtrls;
+  Winapi.Messages, 
+  Winapi.Windows; 
 
 type
   TFormChildTemplate = class abstract(TForm)
+    Pnltop: TPanel;
+    PnlBody: TPanel;
+    Scrollbar: TScrollBar;
+    PnlContents: TPanel;
+    procedure PnlBodyResize(Sender: TObject);
+    procedure ScrollbarScroll(Sender: TObject; ScrollCode: TScrollCode;
+      var ScrollPos: Integer);
+    procedure FormMouseWheelDown(Sender: TObject; Shift: TShiftState;
+      MousePos: TPoint; var Handled: Boolean);
+    procedure FormMouseWheelUp(Sender: TObject; Shift: TShiftState;
+      MousePos: TPoint; var Handled: Boolean);
   public
-    procedure ApplyStyle; virtual; abstract;
+    procedure ApplyStyle; virtual;
     class procedure CreateInto(Parent: TPanel);
   end;
 
@@ -31,7 +43,18 @@ implementation
 
 {$R *.dfm}
 
+uses Algorithms.View.Styles.Colors;
+
 { TFormChildTemplate }
+
+procedure TFormChildTemplate.ApplyStyle;
+begin
+  PnlTop.Color := COLOR_INNER_MENU_BACKGROUND;
+  PnlContents.Color := COLOR_PANEL_BACKGROUND;
+  PnlContents.Top := 0;
+  PnlContents.Left := 0;
+  PnlContents.Width := PnlBody.Width - ScrollBar.Width;
+end;
 
 class procedure TFormChildTemplate.CreateInto(Parent: TPanel);
 begin
@@ -43,6 +66,47 @@ begin
   FormChildTemplate.BorderStyle := bsNone;
   FormChildTemplate.Visible := True;
   FormChildTemplate.ApplyStyle;
+end;
+
+procedure TFormChildTemplate.FormMouseWheelDown(Sender: TObject;
+  Shift: TShiftState; MousePos: TPoint; var Handled: Boolean);
+var
+  I: Integer;
+begin
+  I := ScrollBar.Position + 1;
+  if I <= PnlContents.Height then
+  begin
+    ScrollBar.ScrollBy(0, I);
+    ScrollBar.SetParams(I, 0, 100);
+    ScrollBar.OnScroll(Self, TScrollCode.sclineup, I);
+  end;
+end;
+
+procedure TFormChildTemplate.FormMouseWheelUp(Sender: TObject;
+  Shift: TShiftState; MousePos: TPoint; var Handled: Boolean);
+var
+  I: Integer;
+begin
+  I := ScrollBar.Position - 1;
+  if I >= 0 then
+  begin
+    ScrollBar.ScrollBy(0, I);
+    ScrollBar.SetParams(I, 0, 100);
+    ScrollBar.OnScroll(Self, TScrollCode.sclinedown, I);
+  end;
+end;
+
+procedure TFormChildTemplate.PnlBodyResize(Sender: TObject);
+begin
+  PnlContents.Left := 0;
+  PnlContents.Top := 0;
+  PnlContents.Width := PnlBody.Width - ScrollBar.Width - 1;
+end;
+
+procedure TFormChildTemplate.ScrollbarScroll(Sender: TObject;
+  ScrollCode: TScrollCode; var ScrollPos: Integer);
+begin
+  PnlContents.Top := Round((-PnlContents.Height + PnlBody.Height) / ScrollBar.Max) * ScrollPos;
 end;
 
 end.
