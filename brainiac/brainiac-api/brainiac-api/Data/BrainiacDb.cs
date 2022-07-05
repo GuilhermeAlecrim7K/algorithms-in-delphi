dc76cbee8b6f497e9460ee067ed2d546c3c73def
Yuri Sanford <yurisanford@fortestecnologia.com.br>
2022-07-04 21:39:14 -0300

    Definindo conexão

---
+++ brainiac/brainiac-api/brainiac-api/Data/BrainiacDb.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace brainiac_api.Data
+{
+    public class BrainiacDb : DbContext
+    {
+
+        public BrainiacDb(DbContextOptions<BrainiacDb> options) : base(options)
+        {
+
+        }
+
+    }
+}
